442f273802b63c7353e810b4546a3b1082a56a28
Saurabh Sharma <msaurabh0722@gmail.com>
2023-12-25 23:14:33 +0530

    some changes in src index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// import { Mongoose } from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
- import connectDB from "./db/index.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path : "./env"
